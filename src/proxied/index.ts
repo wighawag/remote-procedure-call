822e401fcf8bfeac80d1e86b89d2d4174067e4d3
wighawag <wighawag@gmail.com>
2024-09-30 07:20:58 +0100

    fixes

--- src/proxied/index.ts
+++ src/proxied/index.ts
@@ -2,48 +2,20 @@ import PromiseThrottle from 'promise-throttle';
 import {Result} from '../types';
 import {call} from '../common';
 
-class JSONRPC {
-	private promiseThrottle: PromiseThrottle | undefined;
-	constructor(
-		protected endpoint: string,
-		options?: {requestsPerSecond?: number},
-	) {
-		if (options?.requestsPerSecond) {
-			this.promiseThrottle = new PromiseThrottle({
-				requestsPerSecond: options.requestsPerSecond,
-				promiseImplementation: Promise,
-			});
-		}
-	}
-
-	call<
-		Method extends string,
-		Value,
-		Error = undefined,
-		Params extends any[] | Record<string, any> | undefined = undefined,
-	>(method: Method): (params: Params extends undefined ? void : Params) => Promise<Result<Value, Error>> {
-		return (params: Params extends undefined ? void : Params) => {
-			if (this.promiseThrottle) {
-				return this.promiseThrottle.add(call.bind(null, this.endpoint, {method, params}));
-			} else {
-				return call<Method, Value, Error, Params>(this.endpoint, {method, params} as any);
-			}
-		};
-	}
-}
-
 export function createJSONRPC<
 	T extends {
-		call<
-			Method extends string,
-			Value,
-			Error = undefined,
-			Params extends any[] | Record<string, any> | undefined = undefined,
-		>(
-			method: Method,
-		): (params: Params extends undefined ? void : Params) => Promise<Result<Value, Error>>;
+		[method: string]: <Value, Error = undefined, Params extends any[] | Record<string, any> | undefined = undefined>(
+			params: Params extends undefined ? void : Params,
+		) => Promise<Result<Value, Error>>;
 	},
 >(endpoint: string, options?: {requestsPerSecond?: number}): T {
+	const promiseThrottle =
+		options?.requestsPerSecond != undefined
+			? new PromiseThrottle({
+					requestsPerSecond: options.requestsPerSecond,
+					promiseImplementation: Promise,
+				})
+			: null;
 	const handler = {
 		get(target, prop, receiver) {
 			const method = prop;
@@ -55,10 +27,10 @@ export function createJSONRPC<
 			>(
 				params: Params extends undefined ? void : Params,
 			) => {
-				if (this.promiseThrottle) {
-					return this.promiseThrottle.add(call.bind(null, this.endpoint, {method, params}));
+				if (promiseThrottle) {
+					return promiseThrottle.add(call.bind(null, endpoint, {method, params}));
 				} else {
-					return call<Method, Value, Error, Params>(this.endpoint, {method, params} as any);
+					return call<Method, Value, Error, Params>(endpoint, {method, params} as any);
 				}
 			};
 		},
