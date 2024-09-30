b4786b8804108261c708916a66685866633601c1
wighawag <wighawag@gmail.com>
2024-09-30 07:24:22 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export {createJSONRPC as createCurriedJSONRPC} from './curried';
-export {createJSONRPC as createProxiedJSONRPC} from './curried';
+export {createJSONRPC as createProxiedJSONRPC} from './proxied';
 export type * from './types';
