077e3059b178f5974459665a569945e813cb244a
wighawag <wighawag@gmail.com>
2024-10-02 10:53:56 +0100

    export CurriedRPC and ProxiedRPC type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export {createJSONRPC as createCurriedJSONRPC} from './curried';
 export {createJSONRPC as createProxiedJSONRPC} from './proxied';
 export type * from './types';
+export type {CurriedRPC} from './curried/types';
+export type {ProxiedRPC} from './proxied/types';
